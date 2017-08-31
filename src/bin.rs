cb0438adb3372e3110d2567d84a1a7de0dae2eb5
Tomas Gavenciak <gavento@ucw.cz>
2017-08-31 16:19:01 +0200

    Formatted bin.rs

--- src/bin.rs
+++ src/bin.rs
@@ -69,16 +69,21 @@ fn run_server(_global_args: &ArgMatches, cmd_args: &ArgMatches) {
 // It checks that 'prefix' exists, but not the full path
 fn make_working_directory(prefix: &Path, base_name: &str) -> Result<PathBuf, String> {
     if !prefix.exists() {
-        return Err(format!("Working directory prefix {:?} does not exists", prefix));
+        return Err(format!(
+            "Working directory prefix {:?} does not exists",
+            prefix
+        ));
     }
 
     if !prefix.is_dir() {
-        return Err(format!("Working directory prefix {:?} is not directory", prefix));
+        return Err(format!(
+            "Working directory prefix {:?} is not directory",
+            prefix
+        ));
     }
 
     let pid = nix::unistd::getpid();
-    let work_dir = prefix.join("rain").join(
-        format!("{}{}", base_name, pid));
+    let work_dir = prefix.join("rain").join(format!("{}{}", base_name, pid));
 
     if work_dir.exists() {
         return Err(format!("Working directory {:?} already exists", work_dir));
@@ -86,8 +91,11 @@ fn make_working_directory(prefix: &Path, base_name: &str) -> Result<PathBuf, Str
 
     debug!("Creating working directory {:?}", work_dir);
     if let Err(e) = std::fs::create_dir_all(work_dir.clone()) {
-        return Err(format!("Working directory {:?} cannot by created: {}",
-                           work_dir, e.description()));
+        return Err(format!(
+            "Working directory {:?} cannot by created: {}",
+            work_dir,
+            e.description()
+        ));
     }
     Ok(work_dir)
 }
@@ -115,14 +123,12 @@ fn run_worker(_global_args: &ArgMatches, cmd_args: &ArgMatches) {
         cpus as u32
     };
 
-    let work_dir_prefix = Path::new(cmd_args.value_of("WORK_DIR")
-        .unwrap_or("/tmp"));
+    let work_dir_prefix = Path::new(cmd_args.value_of("WORK_DIR").unwrap_or("/tmp"));
 
-    let work_dir = make_working_directory(work_dir_prefix, "worker-")
-        .unwrap_or_else(|e| {
-            error!("{}", e);
-            exit(1);
-        });
+    let work_dir = make_working_directory(work_dir_prefix, "worker-").unwrap_or_else(|e| {
+        error!("{}", e);
+        exit(1);
+    });
 
     info!("Starting Rain {} as worker", VERSION);
     info!("Resources: {} cpus", cpus);
@@ -181,14 +187,16 @@ fn main() {
         //(@arg debug: --debug "Enables debug mode (not much effect now - use RUST_LOG)")
         (@subcommand server =>
             (about: "Start a server, waiting for workers and clients.")
-            (@arg LISTEN_ADDRESS: -l --listen +takes_value "Listening port or port/address/address:port (default 0.0.0.0:7210)")
+            (@arg LISTEN_ADDRESS: -l --listen +takes_value
+                "Listening port or port/address/address:port (default 0.0.0.0:7210)")
             (@arg READY_FILE: --ready_file +takes_value
                 "Create a file when server is initialized and ready to accept connections")
             )
         (@subcommand worker =>
             (about: "Start a worker and connect to a given server.")
             (@arg SERVER_ADDRESS: +required "Server address ADDR[:PORT] (default port is 7210)")
-            (@arg LISTEN_ADDRESS: -l --listen +takes_value "Listening port/address/address:port (default = 0.0.0.0:autoassign)")
+            (@arg LISTEN_ADDRESS: -l --listen +takes_value
+                "Listening port/address/address:port (default = 0.0.0.0:autoassign)")
             (@arg CPUS: --cpus +takes_value "Number of cpus (default = autoassign)")
             (@arg WORK_DIR: --workdir +takes_value "Working directory (default = /tmp)")
             (@arg READY_FILE: --ready_file +takes_value
