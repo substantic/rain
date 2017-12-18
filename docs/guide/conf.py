b40d1637e94e1ab3959a874c79b008ac307e1ee3
Vojtech Cima <cima.vojtech@gmail.com>
2017-12-18 19:33:40 +0100

    [CI] Build docs

--- docs/guide/conf.py
+++ docs/guide/conf.py
@@ -38,7 +38,6 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     'sphinxcontrib.napoleon',
-    'sphinxcontrib.fulltoc',
     ]
 
 # Add any paths that contain templates here, relative to this directory.
