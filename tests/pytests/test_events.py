04d59bb2a6db89df9c3b7726ce788b48af10c4dc
Jakub Beránek <berykubik@gmail.com>
2018-04-28 17:39:25 +0200

    [test] add test for session events

---
+++ tests/pytests/test_events.py
@@ -0,0 +1,28 @@
+from time import sleep
+
+import requests
+
+
+def test_event_filter(test_env):
+    test_env.start(1)
+    with test_env.client.new_session() as session:
+        session_id = session.session_id
+
+    sleep(1)
+
+    data = {
+        "event_types": [
+            {"value": "SessionClose", "mode": "="},
+            {"value": "SessionNew", "mode": "="}]
+    }
+
+    res = requests.post('http://localhost:{}/events'.format(test_env.default_http_port), json=data)
+    events = res.json()
+
+    assert len(events) == 2
+    assert events[0]['event']['session'] == session_id
+    assert events[0]['event']['session'] == events[1]['event']['session']
+
+    by_id = sorted(events, key=lambda ev: ev['id'])
+    assert by_id[0]['event']['type'] == 'SessionNew'
+    assert by_id[1]['event']['type'] == 'SessionClose'
