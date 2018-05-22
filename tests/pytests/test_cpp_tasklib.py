a7ea48e89a820c9baadb6ef5eb0e91809495c816
Stanislav Bohm <spirali@kreatrix.org>
2018-05-22 17:03:43 +0200

    [tests] cpp executor testing

--- tests/pytests/test_cpp_tasklib.py
+++ tests/pytests/test_cpp_tasklib.py
@@ -1,87 +1,43 @@
 from rain.client import tasks, blob
 from rain.client import TaskException, Task, Input, Output
 
+from executor_tester import ExecutorTester
+
 import pytest
 
-
-def cpp_hello(obj):
-    return Task("cpptester/hello", inputs=(obj,), outputs=1)
-
-
-def cpp_fail(obj):
-    return Task("cpptester/fail", inputs=(obj,), outputs=0)
-
-
-def cpp_invalid():
-    return
+tester = ExecutorTester("cpptester")
 
 
 def test_cpp_hello_mem(test_env):
-    test_env.start(1, executor="cpptester")
-    with test_env.client.new_session() as s:
-        t1 = cpp_hello(blob("world"))
-        t1.output.keep()
-        s.submit()
-        assert t1.output.fetch().get_bytes() == b"Hello world!"
+    tester.test_hello_mem(test_env)
 
 
 def test_cpp_hello_file(test_env):
-    test_env.start(1, executor="cpptester")
-    with test_env.client.new_session() as s:
-        d1 = blob("WORLD")
-        t0 = tasks.execute("ls",
-                           input_paths=[Input("d1", dataobj=d1)],
-                           output_paths=[Output("d1")])
-        t1 = cpp_hello(t0.output)
-        t1.output.keep()
-        s.submit()
-        assert t1.output.fetch().get_bytes() == b"Hello WORLD!"
+    tester.test_hello_file(test_env)
 
 
 def test_cpp_fail(test_env):
-    test_env.start(1, executor="cpptester")
-    with test_env.client.new_session() as s:
-        t1 = cpp_fail(blob("ABCD"))
-        s.submit()
-        with pytest.raises(TaskException, match='ABCD'):
-            t1.wait()
+    tester.test_fail(test_env)
 
 
 def test_cpp_invalid_inputs(test_env):
-    test_env.start(1, executor="cpptester")
-    with test_env.client.new_session() as s:
-        obj = blob("WORLD")
-        t1 = Task("cpptester/hello", inputs=(obj, obj, obj), outputs=1)
-        s.submit()
-        with pytest.raises(TaskException, match='3'):
-            t1.wait()
+    tester.test_invalid_inputs(test_env)
 
 
 def test_cpp_invalid_outputs(test_env):
-    test_env.start(1, executor="cpptester")
-    with test_env.client.new_session() as s:
-        obj = blob("WORLD")
-        t1 = Task("cpptester/hello", inputs=(obj,), outputs=3)
-        s.submit()
-        with pytest.raises(TaskException, match='3'):
-            t1.wait()
+    tester.test_invalid_outputs(test_env)
 
 
 def test_cpp_invalid(test_env):
-    test_env.start(1, executor="cpptester")
-    with test_env.client.new_session() as s:
-        t1 = Task("cpptester/this_should_not_exist", outputs=0)
-        s.submit()
-        with pytest.raises(TaskException, match='this_should_not_exist'):
-            t1.wait()
+    tester.test_invalid(test_env)
 
 
 def test_cpp_panic(test_env):
-    test_env.start(1, executor="cpptester")
-    with test_env.client.new_session() as s:
-        t1 = Task("cpptester/panic", outputs=0)
-        s.submit()
-        with pytest.raises(TaskException, match='panicked on purpose'):
-            t1.wait()
+    tester.test_panic(test_env)
 
-# TODO: Chain, task burst
+
+def test_cpp_hello_chain(test_env):
+    tester.test_hello_chain(test_env)
+
+def test_cpp_hello_burst(test_env):
+    tester.test_hello_burst(test_env)
