ea3666d18940f899612b86d40b44f289370bf05f
Stanislav Bohm <spirali@kreatrix.org>
2018-05-22 22:39:06 +0200

    [tests] Rust executor testing

--- rain_task_test/src/main.rs
+++ rain_task_test/src/main.rs
@@ -12,9 +12,20 @@ fn task_hello(_ctx: &mut Context, input: &DataInstance, output: &mut Output) -> 
     Ok(())
 }
 
+fn task_fail(_ctx: &mut Context, input: &DataInstance) -> TaskResult<()> {
+    let message : &str = ::std::str::from_utf8(input.get_bytes()?).unwrap_or("Invalid message");
+    Err(message.into())
+}
+
+fn task_panic(_ctx: &mut Context) -> TaskResult<()> {
+    panic!("The task panicked on purpose, by calling task 'panic'");
+}
+
 fn main() {
     env_logger::init();
     let mut s = Executor::new("rusttester");
     register_task!(s, "hello", [I O], task_hello);
+    register_task!(s, "fail", [I], task_fail);
+    register_task!(s, "panic", [], task_panic);
     s.run();
 }
