424bd66937f138723d545cfd01635d5343c1bfcd
Stanislav Bohm <spirali@kreatrix.org>
2018-05-24 15:38:10 +0200

    [governor] Better error messages when executor is terminated

---
+++ tests/pytests/test_executor.py
@@ -0,0 +1,15 @@
+
+# NOTE: Actual executor tests are place in test_XXX_tasklib for each tasklib
+# This is small test for executor machinery itself
+
+from rain.client import TaskException, Task
+import pytest
+
+
+def test_executor_no_registration(test_env):
+    test_env.start(1, executor=("xxx", "/bin/ls"))
+    with test_env.client.new_session() as s:
+        t1 = Task("xxx/abc", inputs=(), outputs=0)
+        s.submit()
+        with pytest.raises(TaskException, match="stdout"):
+          t1.wait()
