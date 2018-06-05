a2f3f08a26307f518068f391edee6a5d42ddd4cb
Tomas Gavenciak <gavento@ucw.cz>
2018-06-06 01:05:20 +0200

    [test][client] Fix attr test

--- tests/pytests/test_attributes.py
+++ tests/pytests/test_attributes.py
@@ -15,15 +15,29 @@ def test_update_attributes(test_env):
         t1 = test()
         s.submit()
         t1.wait()
-        assert "info" not in t1.attributes
+        assert t1.info is None
         t1.update()
-        assert t1.attributes["info"]["governor"].startswith("127.0.0.1:")
+        assert t1.info.governor.startswith("127.0.0.1:")
         t1.update()
-        assert t1.attributes["info"]["governor"].startswith("127.0.0.1:")
-        assert 300 < int(t1.attributes["info"]["duration"]) < 600
-        start = t1.attributes["info"]["start"]
-        # rust sends time in more precision than python parses,
-        # TODO: investigate what is according RFC
-        # for now, lets just trim padding
-        start = start[:start.index(".") + 6]
-        time.strptime(start, '%Y-%m-%dT%H:%M:%S.%f')
+        assert t1.info.governor.startswith("127.0.0.1:")
+        assert 300 < int(t1.info.duration) < 600
+
+
+def test_remote_attributes(test_env):
+
+    @remote()
+    def test(ctx):
+        #ctx. # TODO
+        return b""
+
+    test_env.start(1)
+    with test_env.client.new_session() as s:
+        t1 = test()
+        s.submit()
+        t1.wait()
+        assert t1.info is None
+        t1.update()
+        assert t1.info.governor.startswith("127.0.0.1:")
+        t1.update()
+        assert t1.info.governor.startswith("127.0.0.1:")
+        assert 300 < int(t1.info.duration) < 600
